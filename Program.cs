e24cf540c4694a6b616a6c07865c62ab8e04ad4b
Ezemenahi Benedict <ezemenahi56@gmail.com>
2022-11-09 15:55:56 +0100

    upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+using System.Collections.Generic;
+
+
+Console.Write("Enter Words");
+var strings = Console.ReadLine();
+mylist = new System.Collections.Generic.List<char>();
+Console.WriteLine(mylist);
+foreach (char letter in strings.Split(" "))
+{
+    if (letter.is)
+    {
+        strings.IndexOf(letter);
+    }
+};
+
